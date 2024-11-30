e5a12ee7765fe997aeaf4ebf56e752ad9db8cacb
Pat Deegan <pat.devel@psychogenic.com>
2024-11-30 12:39:09 -0500

    Support for bidir/output enable parameters on all TT DUT ports

--- src/examples/fpga_io/tt_dut.py
+++ src/examples/fpga_io/tt_dut.py
@@ -10,6 +10,10 @@ from microcotb.ports.io import IO
 # get the Simple USB Bridge DUT
 import examples.simple_usb_bridge.dut as sub_dut
 
+import microcotb.log as logging
+
+log = logging.getLogger(__name__)
+
 
 
 class TinyTapeoutDUT(sub_dut.DUT):
@@ -20,7 +24,7 @@ class TinyTapeoutDUT(sub_dut.DUT):
         * clk 
         * rst_n
         * periph1 as an input (reading from demoboard output pmod)
-        * periph2 as an input (reading from demoboard bidir pmod)
+        * periph2 as bidir (read/write from demoboard bidir pmod)
         * host as an output (writing to demoboard input pmod)
         
         We wrap it to give the TT naming ui_in,uio_*,uo_out 
@@ -31,6 +35,23 @@ class TinyTapeoutDUT(sub_dut.DUT):
                  name:str='FPGA', auto_discover:bool=True):
         super().__init__(serial_port, name, auto_discover)
 
+    def discover(self):
+        super().discover()
+        # TT ports are named according to the ASIC's 
+        # perspective, so we want to provide relevant
+        # naming for attributes and
+        #  -- read from uo_out 
+        #  -- allow user config of uio, default to inputs
+        #  -- write to ui_in
+        for oeconf in [('periph1', 'uo_out', 0), ('periph2', 'uio', 0), ('host', 'ui_in', 0xff)]:
+            oename = f'oe_{oeconf[0]}'
+            if hasattr(self, oename):
+                log.debug(f'Have oe port for {oeconf[0]}')
+                oe_port = getattr(self, oename)
+                setattr(self, f'{oeconf[1]}_oe', oe_port)
+                log.info(f'Setting {oename} to {hex(oeconf[2])}')
+                oe_port.value = oeconf[2]
+                
     @property 
     def rst_n(self) -> IO:
         return self.reset
@@ -38,6 +59,7 @@ class TinyTapeoutDUT(sub_dut.DUT):
     @rst_n.setter 
     def rst_n(self, v:int):
         self.reset.value = v
+        
     @property 
     def ui_in(self) -> IO:
         return self.host
