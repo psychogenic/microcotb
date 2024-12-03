9747dfc8f13ce69c48d2657dbd0c32d6a1099fdd
Pat Deegan <pat.devel@psychogenic.com>
2024-12-03 16:26:48 -0500

    Give pin wrappers a name for repr

--- src/microcotb/platform/dummy/pin.py
+++ src/microcotb/platform/dummy/pin.py
@@ -5,9 +5,15 @@ Created on Nov 26, 2024
 @copyright: Copyright (C) 2024 Pat Deegan, https://psychogenic.com
 '''
 class PinWrapper:
-    def __init__(self, pin=None):
+    def __init__(self, name:str, pin=None):
         self._pin = pin 
+        self._name = name
         self._value = 0
+        
+    @property 
+    def name(self):
+        return self._name
+        
         
     @property 
     def value(self):
@@ -16,5 +22,8 @@ class PinWrapper:
     @value.setter 
     def value(self, set_to:int):
         self._value = set_to
+        
+    def __repr__(self):
+        return f'<Pin {self.name}>'
         
 
--- src/microcotb/platform/rp2040/pin.py
+++ src/microcotb/platform/rp2040/pin.py
@@ -8,8 +8,14 @@ Created on Nov 26, 2024
 from machine import Pin
 
 class PinWrapper:
-    def __init__(self, pin):
+    def __init__(self, name:str, pin):
         self._pin = pin 
+        self._name = name 
+        
+    @property 
+    def name(self):
+        return self._name
+        
         
     @property 
     def value(self):
@@ -20,3 +26,6 @@ class PinWrapper:
         #if self._pin.mode != Pin.OUT:
         #    self._pin.mode = Pin.OUT
         self._pin.value(set_to)
+        
+    def __repr__(self):
+        return f'<Pin {self._name}'
